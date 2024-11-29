942755609466b515465ca612e2ef42cc5c6571c4
Minghui Bao <45411947+bbcdabao@users.noreply.github.com>
2024-11-29 15:27:56 +0800

    Update App.java

--- pingmanager/src/main/java/bbcdabao/pingmonitor/pingmanager/App.java
+++ pingmanager/src/main/java/bbcdabao/pingmonitor/pingmanager/App.java
@@ -16,7 +16,7 @@
  *
  */
 
-package bbcdabao.componentsbrz.terminalhub;
+package bbcdabao.pingmonitor.pingmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
