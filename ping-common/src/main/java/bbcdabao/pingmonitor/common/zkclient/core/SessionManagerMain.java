04dd31412aab6a9d983a35f6a02c3415086b4bc4
Minghui Bao <45411947+bbcdabao@users.noreply.github.com>
2024-12-25 14:57:01 +0800

    Create SessionManagerMain.java

---
+++ ping-common/src/main/java/bbcdabao/pingmonitor/common/zkclient/core/SessionManagerMain.java
@@ -0,0 +1,92 @@
+package bbcdabao.pingmonitor.common.zkclient.core;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.CuratorCache;
+
+import bbcdabao.pingmonitor.common.zkclient.BaseEventHandler;
+import bbcdabao.pingmonitor.common.zkclient.core.PathManager.IEventSender;
+import bbcdabao.pingmonitor.common.zkclient.event.ChangedEvent;
+import bbcdabao.pingmonitor.common.zkclient.event.CreatedEvent;
+import bbcdabao.pingmonitor.common.zkclient.event.DeletedEvent;
+import bbcdabao.pingmonitor.common.zkclient.event.IEvent;
+
+public class SessionManagerMain {
+
+    private final ExecutorService executorService;
+
+    private final CuratorFramework client;
+    private final PathManager pathManager;
+
+    private final int qeCapacity = 1000;
+    private final long scanCycle = 2000;
+
+    public SessionManagerMain(ExecutorService executorService, CuratorFramework client, long paybackcycle) {
+        this.executorService = executorService;
+        this.client = client;
+        this.pathManager = new PathManager(client, paybackcycle);
+
+        executorService.execute(pathManager);
+    }
+
+    private class PathSession {
+        private CuratorCache curatorCache;
+        private List<HandlerSend> listSends;
+    }
+
+    private class HandlerSend {
+        protected final AtomicBoolean running = new AtomicBoolean(true);
+        protected final LinkedBlockingQueue<IEvent> eventsQueue = new LinkedBlockingQueue<>(eventQueueSize);
+    }
+    
+    private class HandlerNode implements Runnable, IEventSender {
+        private final WeakReference<BaseEventHandler> weakhandler;
+        private final LinkedBlockingQueue<IEvent> eventsQueue = new LinkedBlockingQueue<>(eventQueueSize);
+
+        private HandlerNode(WeakReference<BaseEventHandler> weakhandler) {
+            this.weakhandler = weakhandler;
+        }
+
+        public void runStep()  throws Exception {
+            IEvent event = eventsQueue.poll(poolTimeOut, TimeUnit.MILLISECONDS);
+            BaseEventHandler handler = weakhandler.get();
+            if (null == handler) {
+                throw new StopException();
+            }
+            if (event instanceof CreatedEvent) {
+                CreatedEvent eventIndex = (CreatedEvent)event;
+                handler.onEvent(eventIndex);
+   
+            }
+            else if (event instanceof ChangedEvent) {
+                ChangedEvent eventIndex = (ChangedEvent)event;
+                handler.onEvent(eventIndex);
+            }
+            else if (event instanceof DeletedEvent) {
+                DeletedEvent eventIndex = (DeletedEvent)event;
+                handler.onEvent(eventIndex);
+            }
+        }
+
+        @Override
+        public void send(IEvent event) {
+            if (!eventsQueue.offer(event)) {
+                // info: log error
+            }
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+
+
+            }
+        } 
+    }
+}
