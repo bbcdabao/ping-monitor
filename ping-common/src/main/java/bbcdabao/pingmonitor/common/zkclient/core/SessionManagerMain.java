6b031c675e36ebd822d4379db9aed32f376bde82
Minghui Bao <45411947+bbcdabao@users.noreply.github.com>
2024-12-26 10:30:33 +0800

    Update SessionManagerMain.java

--- ping-common/src/main/java/bbcdabao/pingmonitor/common/zkclient/core/SessionManagerMain.java
+++ ping-common/src/main/java/bbcdabao/pingmonitor/common/zkclient/core/SessionManagerMain.java
@@ -1,48 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package bbcdabao.pingmonitor.common.zkclient.core;
 
 import java.lang.ref.WeakReference;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.recipes.cache.CuratorCache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ObjectUtils;
 
 import bbcdabao.pingmonitor.common.zkclient.BaseEventHandler;
+import bbcdabao.pingmonitor.common.zkclient.IEventHandlerRegister;
 import bbcdabao.pingmonitor.common.zkclient.core.PathManager.IEventSender;
 import bbcdabao.pingmonitor.common.zkclient.event.ChangedEvent;
 import bbcdabao.pingmonitor.common.zkclient.event.CreatedEvent;
 import bbcdabao.pingmonitor.common.zkclient.event.DeletedEvent;
 import bbcdabao.pingmonitor.common.zkclient.event.IEvent;
 
-public class SessionManagerMain {
+/**
+ * Handle listening sessions for BaseEventHandler
+ */
+public class SessionManagerMain implements IEventHandlerRegister {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SessionManagerMain.class);
 
     private final ExecutorService executorService;
 
     private final CuratorFramework client;
     private final PathManager pathManager;
 
-    private final int qeCapacity = 1000;
-    private final long scanCycle = 2000;
+    private final int qeCapacity;
+    private final long scanCycle;
 
-    public SessionManagerMain(ExecutorService executorService, CuratorFramework client, long paybackcycle) {
+    private SessionManagerMain(
+            ExecutorService executorService,
+            CuratorFramework client,
+            long paybackcycle,
+            int qeCapacity,
+            long scanCycle) {
         this.executorService = executorService;
         this.client = client;
         this.pathManager = new PathManager(client, paybackcycle);
-
+        this.qeCapacity = qeCapacity;
+        this.scanCycle = scanCycle;
         executorService.execute(pathManager);
     }
 
+    /**
+     * For exit 
+     */
+    private static class StopException extends Exception {
+        private static final long serialVersionUID = 1L;
+    }
 
-    
+    private Map<Long, HandlerNode> handlerNodeMap = new HashMap<>(1000);
+    private synchronized HandlerNode getHandlerNode(BaseEventHandler handler) {
+        long code = handler.getCode();
+        HandlerNode handlerNode = handlerNodeMap.computeIfAbsent(code, (k) -> {
+            return new HandlerNode(handler);
+        });
+        return handlerNode;
+    }
+    private synchronized void delHandlerNode(long code) {
+        handlerNodeMap.remove(code);
+    }
+
+    /**
+     * Driver handles events and manages life cycle
+     */
     private class HandlerNode implements Runnable, IEventSender {
         private final WeakReference<BaseEventHandler> weakhandler;
-        private final LinkedBlockingQueue<IEvent> eventsQueue = new LinkedBlockingQueue<>(qeCapacity\);
+        private final long code;
+        private final LinkedBlockingQueue<IEvent> eventsQueue = new LinkedBlockingQueue<>(qeCapacity);
 
-        private HandlerNode(WeakReference<BaseEventHandler> weakhandler) {
-            this.weakhandler = weakhandler;
+        private HandlerNode(BaseEventHandler handler) {
+            this.weakhandler = new WeakReference<>(handler);
+            this.code = handler.getCode();
         }
 
         public void runStep()  throws Exception {
@@ -54,7 +108,6 @@ public class SessionManagerMain {
             if (event instanceof CreatedEvent) {
                 CreatedEvent eventIndex = (CreatedEvent)event;
                 handler.onEvent(eventIndex);
-   
             }
             else if (event instanceof ChangedEvent) {
                 ChangedEvent eventIndex = (ChangedEvent)event;
@@ -69,16 +122,41 @@ public class SessionManagerMain {
         @Override
         public void send(IEvent event) {
             if (!eventsQueue.offer(event)) {
-                // info: log error
+                LOGGER.info("offer event fail");
             }
         }
 
         @Override
         public void run() {
             while (true) {
-
-
+                try {
+                    runStep();
+                } catch (StopException e) {
+                    LOGGER.info("session{} stop exit!", code);
+                    break;
+                } catch (Exception e) {
+                    LOGGER.info("session{} Exception:{}", code, e.getMessage());
+                }
             }
+            delHandlerNode(code);
         } 
+    }
+
+    /**
+     * For out side call
+     * @param path Listen path
+     * @param handler Session handler
+     */
+    @Override
+    public void registerEventHandler(String path, BaseEventHandler handler) {
+        if (ObjectUtils.isEmpty(path)) {
+            return;
+        }
+        if (null == handler) {
+            return;
+        }
+        HandlerNode handlerNode = getHandlerNode(handler);
+        executorService.execute(handlerNode);
+        pathManager.addPathListener(path, handlerNode);
     }
 }
