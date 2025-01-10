dbd59eadb0070b56e9b0648a163cfb44294f52a7
Minghui Bao <45411947+bbcdabao@users.noreply.github.com>
2025-01-10 17:12:39 +0800

    Delete ping-common/src/main/java/bbcdabao/pingmonitor/common/zkclientframe/IEventHandlerRegister.java

--- ping-common/src/main/java/bbcdabao/pingmonitor/common/zkclientframe/IEventHandlerRegister.java
+++
@@ -1,8 +0,0 @@
-package bbcdabao.pingmonitor.common.zkclientframe;
-
-/**
- * For register BaseEventHandler
- */
-public interface IEventHandlerRegister {
-    void registerEventHandler(String path, BaseEventHandler handler);
-}
