2a32c9e1848de63c60b7e79336f166a27529a778
bao <bao_ming_hui@126.com>
2025-02-25 23:45:00 +0800

    modify

--- ping-manager/src/main/java/bbcdabao/pingmonitor/manager/test/Main.java
+++ ping-manager/src/main/java/bbcdabao/pingmonitor/manager/test/Main.java
@@ -1,17 +1,50 @@
 package bbcdabao.pingmonitor.manager.test;
 
-import org.apache.curator.framework.CuratorFramework;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Properties;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
 
+import bbcdabao.pingmonitor.common.dataconver.ByteDataConver;
+import bbcdabao.pingmonitor.common.extraction.ExtractionField;
+import bbcdabao.pingmonitor.common.zkclientframe.core.CuratorFrameworkInstance;
+
 @Service
 public class Main extends Thread implements ApplicationRunner {
-    //@Autowired
-    //private CuratorFramework curatorFramework;
     @Override
     public void run(ApplicationArguments args) throws Exception {
         start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(2000);
+        } catch (Exception e) {
+
+        }
+        MyPlug myPlug = new MyPlug();
+        myPlug.setMyid(3000);
+        myPlug.setInfo("fuck!");
+        Properties properties = ExtractionField.getInstance().extractionPropertiesFromObject(myPlug);
+
+        byte[] propertb = ByteDataConver.getInstance().getConvertToByteForProperties().getData(properties);
+
+        try {
+            CuratorFrameworkInstance.getInstance().create().creatingParentsIfNeeded().forPath("/nmb", propertb);
+            byte[] propertread = CuratorFrameworkInstance.getInstance().getData().forPath("/nmb");
+            Properties aaa = ByteDataConver.getInstance().getConvertFromByteForProperties().getValue(propertread);
+            int a;
+            a = 9;
+        } catch (Exception e) {
+            int a;
+            a = 9;
+        }
+
+        MyPlug myPlugs = new MyPlug();
+        ExtractionField.getInstance().populateObjectFromProperties(properties, myPlugs);
+        int a;
+        a = 9;
     }
 }
