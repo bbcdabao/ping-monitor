4eb70f070b4d123dae036b7e8e4fa56b9e09381a
bao <bao_ming_hui@126.com>
2025-02-28 23:44:53 +0800

    modify

--- ping-manager/src/main/java/bbcdabao/pingmonitor/manager/test/Main.java
+++ ping-manager/src/main/java/bbcdabao/pingmonitor/manager/test/Main.java
@@ -1,14 +1,8 @@
 package bbcdabao.pingmonitor.manager.test;
 
-import java.util.Properties;
-
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
-
-import bbcdabao.pingmonitor.common.dataconver.ByteDataConver;
-import bbcdabao.pingmonitor.common.extraction.ExtractionField;
-import bbcdabao.pingmonitor.common.zkclientframe.core.CuratorFrameworkInstance;
 
 @Service
 public class Main extends Thread implements ApplicationRunner {
@@ -19,32 +13,5 @@ public class Main extends Thread implements ApplicationRunner {
 
     @Override
     public void run() {
-        try {
-            Thread.sleep(2000);
-        } catch (Exception e) {
-
-        }
-        MyPlug myPlug = new MyPlug();
-        myPlug.setMyid(3000);
-        myPlug.setInfo("fuck!");
-        Properties properties = ExtractionField.getInstance().extractionPropertiesFromObject(myPlug);
-
-        byte[] propertb = ByteDataConver.getInstance().getConvertToByteForProperties().getData(properties);
-
-        try {
-            CuratorFrameworkInstance.getInstance().create().creatingParentsIfNeeded().forPath("/nmb", propertb);
-            byte[] propertread = CuratorFrameworkInstance.getInstance().getData().forPath("/nmb");
-            Properties aaa = ByteDataConver.getInstance().getConvertFromByteForProperties().getValue(propertread);
-            int a;
-            a = 9;
-        } catch (Exception e) {
-            int a;
-            a = 9;
-        }
-
-        MyPlug myPlugs = new MyPlug();
-        ExtractionField.getInstance().populateObjectFromProperties(properties, myPlugs);
-        int a;
-        a = 9;
     }
 }
