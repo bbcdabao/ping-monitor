8f802ba67b59c5120858990f8577542d6dbcd361
Minghui Bao <45411947+bbcdabao@users.noreply.github.com>
2025-03-05 17:42:35 +0800

    Update MasterKeeperTaskManager.java

--- ping-common/src/main/java/bbcdabao/pingmonitor/common/coordination/MasterKeeperTaskManager.java
+++ ping-common/src/main/java/bbcdabao/pingmonitor/common/coordination/MasterKeeperTaskManager.java
@@ -18,6 +18,98 @@
 
 package bbcdabao.pingmonitor.common.coordination;
 
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+import org.apache.curator.framework.recipes.leader.LeaderLatch;
+
+import bbcdabao.pingmonitor.common.config.ExecutorConfig;
+import bbcdabao.pingmonitor.common.zkclientframe.core.CuratorFrameworkInstance;
+
+/**
+ * Master keep run
+ */
+public class MasterKeeperTaskManager {
+
+    private static class Holder {
+        private static final MasterKeeperTaskManager INSTANCE = new MasterKeeperTaskManager();
+    }
+
+    public static MasterKeeperTaskManager getInstance() {
+        return Holder.INSTANCE;
+    }
+
+    private MasterKeeperTaskManager() {
+    }
+
+    private void sleepStep() {
+        try {
+            Thread.sleep(000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void selectMasterRun(String path, Runnable runnable, Consumer<Exception> errorHandler) throws Exception {
+        ExecutorConfig.getInstance().execute(() -> {
+            LeaderLatch leaderLatch = new LeaderLatch(CuratorFrameworkInstance.getInstance(), path);
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    leaderLatch.start();
+                    break;
+                } catch (Exception e) {
+                    if (null != errorHandler) {
+                        errorHandler.accept(e);
+                    } else {
+                        e.printStackTrace();
+                    }
+                    sleepStep();
+                }
+            }
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    if (leaderLatch.hasLeadership()) {
+                        runnable.run();
+                    } else {
+                        leaderLatch.await(5, TimeUnit.SECONDS);
+                    }
+                } catch (Exception e) {
+                    if (null != errorHandler) {
+                        errorHandler.accept(e);
+                    } else {
+                        e.printStackTrace();
+                    }
+                    sleepStep();
+                }
+            }
+            try {
+                leaderLatch.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package bbcdabao.pingmonitor.common.coordination;
+
 /**
  * Master keep run
  */
