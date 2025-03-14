dbcdea967e21ed7b18ad42171f770a0c8bccd0c4
Minghui Bao <45411947+bbcdabao@users.noreply.github.com>
2025-03-14 15:31:46 +0800

    Update RobotManagerController.java

--- ping-manager/src/main/java/bbcdabao/pingmonitor/manager/app/controller/RobotManagerController.java
+++ ping-manager/src/main/java/bbcdabao/pingmonitor/manager/app/controller/RobotManagerController.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package bbcdabao.pingmonitor.manager.app.controller;
 
 import java.io.IOException;
@@ -11,7 +29,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import bbcdabao.pingmonitor.manager.app.domain.handlers.RobotInfosHandler;
+import bbcdabao.pingmonitor.manager.app.domain.sse.processes.InstancesProcess;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
@@ -20,23 +38,13 @@ public class RobotManagerController {
 
     private final Logger logger = LoggerFactory.getLogger(RobotManagerController.class);
 
-    @GetMapping(value = "{robotGroupName}/infos", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(value = "/{robotGroupName}/instances", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @ResponseBody
-    public void getInfos(@PathVariable("robotGroupName") String robotGroupName,
+    public void getinstances(
+            @PathVariable("robotGroupName") String robotGroupName,
             HttpServletResponse response) throws IOException {
-
-        RobotInfosHandler robotInfosHandler = new RobotInfosHandler(robotGroupName, response);
-        
-        while (true) {
-            if (robotInfosHandler.isOver()) {
-                break;
-            }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                break;
-            }
-            logger.info("sssddd");
-        }
+        logger.info("enter:getinstances:{}", robotGroupName);
+        InstancesProcess instancesProcess = new InstancesProcess(robotGroupName, response);
+        instancesProcess.doRun();
     }
 }
