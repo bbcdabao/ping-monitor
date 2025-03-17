9e7de786e839ae1addf1e9634f582a59914a09f1
Minghui Bao <45411947+bbcdabao@users.noreply.github.com>
2025-03-17 14:45:35 +0800

    Create RobotInstanceInfo.java

---
+++ ping-manager/src/main/java/bbcdabao/pingmonitor/manager/app/module/RobotInstanceInfo.java
@@ -0,0 +1,9 @@
+package bbcdabao.pingmonitor.manager.app.module;
+
+import lombok.Data;
+
+@Data
+public class RobotInstanceInfo {
+    private String robotId;
+    private String robotInfo = "none";
+}
