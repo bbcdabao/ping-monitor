0253814f3d31a2236ce3c784e6991d8f18734bfd
Minghui Bao <45411947+bbcdabao@users.noreply.github.com>
2025-03-21 17:41:24 +0800

    Update PingWorkerService.java

--- ping-robot/ping-robot-api/src/main/java/bbcdabao/pingmonitor/pingrobotapi/app/services/impl/PingWorkerService.java
+++ ping-robot/ping-robot-api/src/main/java/bbcdabao/pingmonitor/pingrobotapi/app/services/impl/PingWorkerService.java
@@ -18,15 +18,21 @@
 
 package bbcdabao.pingmonitor.pingrobotapi.app.services.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
-import bbcdabao.pingmonitor.common.domain.FactoryBase;
+import bbcdabao.pingmonitor.common.domain.PingmonitorExecutor;
 import bbcdabao.pingmonitor.common.domain.coordination.CoordinationManager;
 import bbcdabao.pingmonitor.common.domain.coordination.IPath;
 import bbcdabao.pingmonitor.common.domain.coordination.Sysconfig;
@@ -34,6 +40,7 @@ import bbcdabao.pingmonitor.common.domain.zkclientframe.BaseEventHandler;
 import bbcdabao.pingmonitor.common.domain.zkclientframe.event.ChangedEvent;
 import bbcdabao.pingmonitor.common.domain.zkclientframe.event.CreatedEvent;
 import bbcdabao.pingmonitor.common.domain.zkclientframe.event.DeletedEvent;
+import bbcdabao.pingmonitor.pingrobotapi.IPingMoniterPlug;
 import bbcdabao.pingmonitor.pingrobotapi.app.services.ISysconfig;
 import bbcdabao.pingmonitor.pingrobotapi.app.services.ISysconfigNotify;
 import bbcdabao.pingmonitor.pingrobotapi.domain.RobotConfig;
@@ -43,53 +50,98 @@ public class PingWorkerService extends TimeWorkerBase implements ApplicationRunn
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PingWorkerService.class);
 
+    /**
+     * Monitor changes
+     */
     private class MonitorChange extends BaseEventHandler {
         @Override
         public void onEvent(CreatedEvent data) throws Exception {
             isReMake.set(true);
+            LOGGER.info("PingWorkerService.MonitorChange.CreatedEvent");
         }
         @Override
         public void onEvent(ChangedEvent data) throws Exception {
             isReMake.set(true);
+            LOGGER.info("PingWorkerService.MonitorChange.ChangedEvent");
         }
         @Override
         public void onEvent(DeletedEvent data) throws Exception {
             isReMake.set(true);
+            LOGGER.info("PingWorkerService.MonitorChange.DeletedEvent");
         }
     }
 
     @Autowired
     private ISysconfig regSysconfigNotify;
 
+    @Autowired
+    private RobotConfig robotConfig;
+
     private MonitorChange monitorChange = new MonitorChange();
+
     private AtomicBoolean isReMake = new AtomicBoolean(true);
 
-    private void doReMake() throws Exception {
+    private Map<String, PlugInfo> plugInfoMap = new HashMap<>(100);
+
+    private IPath robotRunInfoTaskPath;
+    private String pathMonitor;
+
+    private class PlugInfo {
+        private String taskName;
+        /**
+         * system update
+         */
+        private long sytime;
+        /**
+         * task config update
+         */
+        private long mtime;
+        private IPingMoniterPlug plug;
+    }
+
+    private void checkRemove() {
         CoordinationManager cm = CoordinationManager.getInstance();
-        String robotGroupName = FactoryBase
-                .getFactory()
-                .getBean(RobotConfig.class).getRobotGroupName();
-        cm.getChildren(IPath.robotRunInfoTaskPath(robotGroupName, IPath.REG_UUID));
-        
+        Iterator<Map.Entry<String, PlugInfo>> iterator = plugInfoMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, PlugInfo> entry = iterator.next();
+            PlugInfo plugInfo = entry.getValue();
+            try {
+                Stat stat = cm.getStat(IPath.taskConfigPath(plugInfo.taskName));
+                if (stat.getMtime() != plugInfo.mtime) {
+                    iterator.remove();
+                }
+            } catch (Exception e) {
+                LOGGER.info("PingWorkerService-checkRemove Exception:{}", e.getMessage());
+            }
+        }
+    }
+
+    private List<String> rtasks = new ArrayList<>();
+    private void doReMake() {
+        LOGGER.info("PingWorkerService-doReMake Enter");
+        CoordinationManager cm = CoordinationManager.getInstance();
+        try {
+            rtasks = cm.getChildren(robotRunInfoTaskPath);
+        } catch (Exception e) {
+            LOGGER.info("PingWorkerService-doReMake Exception:{}", e.getMessage());
+        }
     }
 
     @PostConstruct
     public void init() {
         regSysconfigNotify.reg(this);
-    }
-
-    @Override
-    public void onChange(Sysconfig config) throws Exception {
-        beginCron(config.getCron());
+        robotRunInfoTaskPath = IPath.robotRunInfoTaskPath(robotConfig.getRobotGroupName(), IPath.REG_UUID);
+        pathMonitor = "NODE:" + robotRunInfoTaskPath.get();
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        String robotGroupName = FactoryBase
-                .getFactory()
-                .getBean(RobotConfig.class).getRobotGroupName();
-        monitorChange.start("NODE:" +
-        IPath.robotRunInfoTaskPath(robotGroupName, IPath.REG_UUID).get());
+        monitorChange.start(pathMonitor);
+    }
+
+    @Override
+    public void onChange(Sysconfig config) throws Exception {
+        beginCron(config.getCronTask());
     }
 
     @Override
@@ -98,5 +150,8 @@ public class PingWorkerService extends TimeWorkerBase implements ApplicationRunn
             doReMake();
             isReMake.set(false);
         }
+        PingmonitorExecutor.getInstance().execute(() -> {
+            
+        });
     }
 }
