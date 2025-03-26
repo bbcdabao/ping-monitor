766ead1f1673641fa69b03e7d9d57dcf81a34c65
Minghui Bao <45411947+bbcdabao@users.noreply.github.com>
2025-03-26 11:45:24 +0800

    Update ProcessExceptionHandler.java

--- ping-manager/src/main/java/bbcdabao/pingmonitor/manager/app/controller/ProcessExceptionHandler.java
+++ ping-manager/src/main/java/bbcdabao/pingmonitor/manager/app/controller/ProcessExceptionHandler.java
@@ -23,11 +23,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-//@ControllerAdvice
+@ControllerAdvice
 public class ProcessExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Internal server error: " + e.getMessage());
     }
 }
