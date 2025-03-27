49a4bd7ffe715c3a600316695c16e10745a63b84
Minghui Bao <45411947+bbcdabao@users.noreply.github.com>
2025-03-27 11:43:08 +0800

    Update language.vue

--- ping-manager-ui/src/components/language.vue
+++ ping-manager-ui/src/components/language.vue
@@ -3,14 +3,14 @@
     <div>
         <el-dropdown trigger="click" @command="handleLangCommand">
             <span class="el-dropdown-link">
-                {{ shwoLangs[idxLanguage] }}
+                {{ showLangs[idxLanguage] }}
                 <el-icon class="el-icon--right">
                     <arrow-down />
                 </el-icon>
             </span>
             <template #dropdown>
                 <el-dropdown-menu>
-                    <el-dropdown-item v-for="(item, index) in shwoLangs" divided :command="index" :key="index">
+                    <el-dropdown-item v-for="(item, index) in showLangs" divided :command="index" :key="index">
                         {{ item }}
                     </el-dropdown-item>
                 </el-dropdown-menu>
@@ -19,22 +19,22 @@
     </div>
 </template>
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { defineComponent, ref, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 
 const { t, locale } = useI18n();
 
-const shwoLangs: any = {};
-shwoLangs['en'] = 'EN';
-shwoLangs['zh'] = '中文';
+const showLangs: Record<string, string> = {};
+showLangs['en'] = 'EN';
+showLangs['zh'] = '中文';
 
 const getLanguage = () => {
-    locale.value = localStorage.getItem('cmd-terminal-lang') as 'en' | 'zh' || 'en';
+    locale.value = localStorage.getItem('ping-monitor-lang') as 'en' | 'zh' || 'en';
     return locale.value;
 }
 const idxLanguage = ref(getLanguage());
 const handleLangCommand = (command: string) => {
-    localStorage.setItem('cmd-terminal-lang', command);
+    localStorage.setItem('ping-monitor-lang', command);
     idxLanguage.value = getLanguage();
 }
 
