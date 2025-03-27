778ceb6bd191b8ecbf95001768d02ced49830224
Minghui Bao <45411947+bbcdabao@users.noreply.github.com>
2025-03-27 10:28:54 +0800

    Update main.ts

--- ping-manager-ui/src/main.ts
+++ ping-manager-ui/src/main.ts
@@ -1,18 +1,22 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
-import App from './App.vue';
+
+import { createApp } from 'vue';
+import App from '@/App.vue';
+import { createPinia } from 'pinia';
 import router from '@/router';
+import i18n from '@/i18n';
+
 import 'element-plus/dist/index.css';
 import '@/assets/css/icon.css';
-import i18n from '@/i18n';
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(i18n);
 
-// 注册elementplus图标
+/**
+ * Register elementplus
+ */
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component);
 }
